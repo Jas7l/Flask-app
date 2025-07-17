99e4a188aab52ef84babe9ecba0283972d8662dc
Jas7l <jasilhasil1@gmail.com>
2025-07-17 10:22:21 +0300

    login by id, update active logic

--- backend/service/user_manager.py
+++ backend/service/user_manager.py
@@ -18,40 +18,35 @@ class UserManager:
         with get_db() as db:
             user = db.query(User).filter(User.login == login_input).first()
 
+            if user.active is False:
+                return jsonify({"message": "User is inactive"}), HTTPStatus.FORBIDDEN
+
             if not user or not bcrypt.checkpw(password_input.encode(), user.password):
                 return jsonify({"message": "Missing login or password"}), HTTPStatus.BAD_REQUEST
 
             login_user(user)
-            user.active = True
-            db.commit()
-            db.refresh(user)
             return jsonify({"message": "Login successful"}), HTTPStatus.OK
 
     @staticmethod
     def logout():
-        with get_db() as db:
-            current_user.active = False
-            db.add(current_user)
-            db.commit()
-            db.refresh(current_user)
         logout_user()
         return jsonify({"message": "Logged out"}), HTTPStatus.OK
 
     @staticmethod
     def user_info():
-        login = current_user.login
-        result = UserManager.user_info_by_login(login)
+        input_id = int(current_user.orbis_id)
+        result = UserManager.user_info_by_id(input_id)
         if not result:
             return jsonify({"message": "User not found"}), HTTPStatus.NOT_FOUND
         return jsonify(result), HTTPStatus.OK
 
     @staticmethod
-    def user_info_by_login(login_input: str):
+    def user_info_by_id(input_id: int):
         with get_db() as db:
             user_table = Table('users', MetaData(), autoload_with=db.bind)
 
             columns = [col for col in user_table.columns if col.name != "password"]
-            req = select(*columns).where(user_table.c.login == login_input)
+            req = select(*columns).where(user_table.c.orbis_id == input_id)
             result = db.execute(req).first()
 
             if not result:
